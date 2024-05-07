d65375c886ee4299dead2aec3941470f415b6ab3
ikhsangama <ikhsangama@gmail.com>
2024-05-07 14:22:57 +0700

    feat: submit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import Logo from "/logo.svg";
 import "./App.css";
+import {useEffect, useState} from "react";
+import {User} from "./models/User.tsx";
+import {UserComponent} from "./components/User.tsx";
 
+const fetchUser = async () => {
+  return await fetch('https://private-e843bc-betamindhiring.apiary-mock.com/users/1/maybe_friends', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then((res) => res.json())
+    .catch(err => console.log(err))
+}
 const App = () => {
+  const [user, setUser] = useState<User>();
+  useEffect(() => {
+    fetchUser()
+      .then((data) => {
+        setUser(data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      })
+  }, []);
+
+  if (!user) {
+    return <h1>Loading...</h1>;
+  }
   return (
     <>
       <div>
@@ -11,11 +37,11 @@ const App = () => {
       <div>
         <h3>Maybe friends?</h3>
         {/*
-          TODO: show people who may be friends
-
-          you can get date from the below endpoint:
-          https://private-e843bc-betamindhiring.apiary-mock.com/users/1/maybe_friends
-        */}
+        TODO: show people who may be friends
+        you can get date from the below endpoint:
+        https://private-e843bc-betamindhiring.apiary-mock.com/users/1/maybe_friends
+      */}
+        <UserComponent user={user}/>
       </div>
     </>
   );
