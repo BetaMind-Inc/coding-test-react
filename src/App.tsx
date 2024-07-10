a1baba7f6e34e58876003692b5dfd88c788972f6
kousaku-maron <kousaku.maron@gmail.com>
2024-07-10 15:35:34 +0900

    🧩 add util components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import Logo from "/logo.svg";
 import "./App.css";
+import Error from "./components/Error";
+import Loading from "./components/Loading";
 
 const App = () => {
   return (
@@ -11,8 +13,8 @@ const App = () => {
       <div>
         <h3>Maybe friends?</h3>
         {/*
-          TODO: show people who may be friends
-
+          TODO: show uniq view seconds, and pls use <Loading /> and <Error />
+        
           you can get date from the below endpoint:
           https://private-e843bc-betamindhiring.apiary-mock.com/users/1/maybe_friends
         */}
